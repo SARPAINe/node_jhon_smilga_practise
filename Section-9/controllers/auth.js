8440538413c0e3c9f3728e47db0aeab45075f315
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2023-08-22 16:55:53 +0600

    ongoing

--- Section-9/controllers/auth.js
+++ Section-9/controllers/auth.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const StatusCodes = require("http-status-codes");
+const { BadRequestError, UnauthenticatedError } = require("../errors/index");
 
 const register = async (req, res) => {
     const user = await User.create({ ...req.body });
@@ -8,7 +9,25 @@ const register = async (req, res) => {
 };
 
 const login = async (req, res) => {
-    res.status(200).json({ msg: "login user" });
+    const { email, password } = req.body;
+    if (!email || !password) {
+        throw new BadRequestError("Please provide email and password");
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new UnauthenticatedError("No such user found");
+    }
+
+    const isPasswordCorrect = await user.comparePassword(password);
+    if (!isPasswordCorrect) {
+        throw new UnauthenticatedError("Wrong password");
+    }
+
+    const token = user.createJWT();
+
+    res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
 };
 
 module.exports = { register, login };
