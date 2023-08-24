df398e33b394489307ea999b73f19cf1e0f54764
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2023-08-24 10:04:13 +0600

    added swagger documentation

--- Section-9/app.js
+++ Section-9/app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 require("express-async-errors");
+
 const express = require("express");
 const app = express();
 
@@ -8,6 +9,11 @@ const helmet = require("helmet");
 const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
+
+//Swagger
+const swaggerUI = require("swagger-ui-express");
+const YAML = require("yamljs");
+const swaggerDocument = YAML.load("./swagger.yaml");
 
 //connectDB
 const connectDB = require("./db/connect");
@@ -21,13 +27,13 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(
-  rateLimiter({
-    windowMs: 60 * 1000, // 1 minutes
-    max: 2, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
-    // store: ... , // Use an external store for more precise rate limiting
-  })
+    rateLimiter({
+        windowMs: 60 * 1000, // 1 minutes
+        max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+        standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+        legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+        // store: ... , // Use an external store for more precise rate limiting
+    })
 );
 app.use(express.json());
 app.use(helmet());
@@ -35,6 +41,10 @@ app.use(cors());
 app.use(xss());
 // extra packages
 
+app.get("/", (req, res) => {
+    res.send('<h1>jobs API</h1><a href="/api-docs">Documentation</>');
+});
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // routes
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
@@ -45,14 +55,14 @@ app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 3000;
 
 const start = async () => {
-  try {
-    await connectDB(process.env.MONGO_URI);
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () =>
+            console.log(`Server is listening on port ${port}...`)
+        );
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 start();
