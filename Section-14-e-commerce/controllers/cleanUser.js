e0bf12643781bcce2a1d5c7b6899c52516c237bb
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2023-09-08 17:47:46 +0600

    auth remains

--- Section-14-e-commerce/controllers/cleanUser.js
+++
@@ -1,8 +0,0 @@
-const User = require("../models/User");
-const StatusCodes = require("http-status-codes");
-const deleteUsers = async (req, res) => {
-    const resp = await User.deleteMany({});
-    res.status(StatusCodes.OK).json(resp);
-};
-
-module.exports = { deleteUsers };
--- Section-14-e-commerce/routes/cleanDBRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express();
-const { deleteUsers } = require("../controllers/cleanUser");
-
-router.delete("/users", deleteUsers);
-module.exports = router;
