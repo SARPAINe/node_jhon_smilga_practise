9c07a6dc064ffdd106687b58c1193eb0bcde8f3b
Shaharin Ahmed <ahmedshaharin7589@gmail.com>
2023-09-10 12:35:44 +0600

    Account verification email complete

--- Section-14-e-commerce/models/User.js
+++ Section-14-e-commerce/models/User.js
@@ -3,47 +3,51 @@ const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please provide name"],
-        minlength: 3,
-        maxlength: 50,
+  name: {
+    type: String,
+    required: [true, "Please provide name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: [true, "Please provide email"],
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
     },
-    email: {
-        type: String,
-        unique: true,
-        required: [true, "Please provide email"],
-        validate: {
-            validator: validator.isEmail,
-            message: "Please provide valid email",
-        },
-    },
-    password: {
-        type: String,
-        required: [true, "Please provide password"],
-        minlength: 6,
-    },
-    role: {
-        type: String,
-        enum: ["admin", "user"],
-        default: "user",
-    },
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minlength: 6,
+  },
+  role: {
+    type: String,
+    enum: ["admin", "user"],
+    default: "user",
+  },
+  verificationToken: String,
+  isVerified: { type: Boolean, default: false },
+  verified: Date,
 });
 
 UserSchema.pre("save", async function () {
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
+  if (!this.isModified("password")) return;
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 UserSchema.pre("remove", async function (next) {
-    console.log("remove hook");
-    await this.model("Product").deleteMany({ user: this._id });
-    next();
+  console.log("remove hook");
+  await this.model("Product").deleteMany({ user: this._id });
+  next();
 });
 
 UserSchema.methods.comparePassword = async function (candidatePassword) {
-    const isMatch = await bcrypt.compare(candidatePassword, this.password);
-    return isMatch;
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
 };
 
 module.exports = mongoose.model("User", UserSchema);
