c3fd638ae6f7abf72c07b842e872c89d71fd1d3c
Shaharin Ahmed <shaharin.ahmed@bjitgroup.com>
2023-09-14 16:16:10 +0600

    fixed updateUser cookies, refresh token was not attached

--- Section-14-e-commerce/utils/jwt.js
+++ Section-14-e-commerce/utils/jwt.js
@@ -1,33 +1,34 @@
 const jwt = require("jsonwebtoken");
 
 const createJWT = ({ payload }) => {
-  const token = jwt.sign(payload, process.env.JWT_SECRET);
-  return token;
+    const token = jwt.sign(payload, process.env.JWT_SECRET);
+    return token;
 };
 
 const isTokenValid = (token) => jwt.verify(token, process.env.JWT_SECRET);
 
 const attachCookiesToResponse = ({ res, user, refreshToken }) => {
-  const accessTokenJWT = createJWT({ payload: { user } });
-  const refreshTokenJWT = createJWT({ payload: { user, refreshToken } });
+    console.log(refreshToken);
+    const accessTokenJWT = createJWT({ payload: { user } });
+    const refreshTokenJWT = createJWT({ payload: { user, refreshToken } });
 
-  const oneDay = 1000 * 60 * 60 * 24;
-  const longerExp = 1000 * 60 * 60 * 24 * 30;
+    const oneDay = 1000 * 60 * 60 * 24;
+    const longerExp = 1000 * 60 * 60 * 24 * 30;
 
-  res.cookie("accessToken", accessTokenJWT, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    signed: true,
-    expires: new Date(Date.now() + oneDay),
-    // maxAge: 5000,
-  });
+    res.cookie("accessToken", accessTokenJWT, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        signed: true,
+        // expires: new Date(Date.now() + oneDay),
+        maxAge: 5000,
+    });
 
-  res.cookie("refreshToken", refreshTokenJWT, {
-    httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    signed: true,
-    expires: new Date(Date.now() + longerExp),
-  });
+    res.cookie("refreshToken", refreshTokenJWT, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        signed: true,
+        expires: new Date(Date.now() + longerExp),
+    });
 };
 // const attachSingleCookieToResponse = ({ res, user }) => {
 //   const token = createJWT({ payload: user });
@@ -43,7 +44,7 @@ const attachCookiesToResponse = ({ res, user, refreshToken }) => {
 // };
 
 module.exports = {
-  createJWT,
-  isTokenValid,
-  attachCookiesToResponse,
+    createJWT,
+    isTokenValid,
+    attachCookiesToResponse,
 };
